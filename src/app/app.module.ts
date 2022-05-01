1ab1ea4f2c2fc229fe51dd4642c9139141aa858f
marcelo vinicius <viniciusmarcello12@gmail.com>
2022-05-01 15:43:08 -0300

    exibindo valores no console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     novaTranferenciaComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [  //utlizado para importar elementos de uma biblioteca
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,13 +1,22 @@
 import { Component } from "@angular/core";
 
+
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
 
 })
 export class novaTranferenciaComponent{
 
+  valor : number | undefined ;
+  destino: number | undefined;
 
+
+  transferir() {
+    console.log("transferencia realizada");
+    console.log('valor', this.valor)
+    console.log('destino', this.destino);
+  }
 
 }
